e45a60762b97cab961d7ee3f25d3e801c379e707
23rd <23rd@vivaldi.net>
2021-08-07 07:19:10 +0300

    Updated packaging in Github CI.

--- win_standalone.bat
+++
@@ -1,3 +0,0 @@
-( gradlew windowsZip -PjavapackagerPath="c:\java8\bin\javapackager.exe"
-gradlew innosetup -PinnosetupPath="c:\Program Files (x86)\Inno Setup 6\ISCC.exe"
-gradlew innosetupStandalone -PinnosetupPath="c:\Program Files (x86)\Inno Setup 6\ISCC.exe" -PjavapackagerPath="c:\java8\bin\javapackager.exe")
